46c054e7df1664294a13971108a5ffc89b7cce76
regosso <regosso@ukr.net>
2017-09-20 13:20:34 +0300

    T rails db migrate

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170830150849) do
+ActiveRecord::Schema.define(version: 20170920091238) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
